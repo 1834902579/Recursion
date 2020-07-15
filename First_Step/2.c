185f3cbcf7e0adaf3b6de39436a2064535559c30
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-07-14 22:11:30 -0700

    Update 2.c

--- First_Step/2.c
+++ First_Step/2.c
@@ -2,16 +2,13 @@
 
 int stepCount = 0 ;
 int SumofPow(int a , int b );
-int Fastpow(int a, int b );
+
 
 int main()
 {
     printf("%d",SumofPow(4,4));
     printf("\nstep : %d",stepCount);
-     printf("\n%d",Fastpow(4,24));
-    printf("\nstep : %d",stepCount);
-
-
+ 
     return 0 ;
 }
  int SumofPow(int a , int b )
@@ -23,20 +20,3 @@ int main()
 
     return a* SumofPow(a,b-1);
 }
-int Fastpow(int a , int b){
-
-   stepCount ++;
-    if(b==0){
-        return 1 ;
-
-    }
-
-    if(b%2==0){
-        return Fastpow(a*a,b/2);
-
-        }
-        return a * Fastpow(a,b-1);
-
-
-}
-
